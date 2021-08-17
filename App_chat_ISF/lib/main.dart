edd2cd1c155f72079105b0174e368960a34f1a8f
Mohammed Amine ISFOULA <medamineisf@gmail.com>
2021-08-17 15:20:20 +0100

    9 no

--- App_chat_ISF/lib/main.dart
+++ App_chat_ISF/lib/main.dart
@@ -1,11 +1,13 @@
 //@dart=2.3
 import 'package:flutter/material.dart';
-import 'package:app_chat/composition_root.dart';
+//import 'package:app_chat/composition_root.dart';
 import 'package:app_chat/theme.dart';
+import 'package:app_chat/ui/pages/onboarding/onboarding.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await CompositionRoot.configure();
+void main() {
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await CompositionRoot.configure();
   runApp(MyApp());
   // final firstPage = CompositionRoot.start();
   // runApp(MyApp(firstPage));
@@ -23,7 +25,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightTheme(context),
       darkTheme: darkTheme(context),
-      home: CompositionRoot.composeOnboardingUi(),
+       home: Onboarding(),
+      // home: CompositionRoot.composeOnboardingUi(),
       // home: CompositionRoot.composeHomeUi(),
       // home: firstPage,
     );
--- App_chat_ISF/lib/states_management/onboarding/onboarding_cubit.dart
+++ App_chat_ISF/lib/states_management/onboarding/onboarding_cubit.dart
@@ -1,37 +1,37 @@
-//@dart=2.3
-import 'dart:io';
+// //@dart=2.3
+// import 'dart:io';
 
-import 'package:chat/chat.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-//import 'package:app_chat/cache/local_cache.dart';
-import 'package:app_chat/data/services/image_uploader.dart';
-import 'package:app_chat/states_management/onboarding/onboarding_state.dart';
+// import 'package:chat/chat.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// //import 'package:app_chat/cache/local_cache.dart';
+// import 'package:app_chat/data/services/image_uploader.dart';
+// import 'package:app_chat/states_management/onboarding/onboarding_state.dart';
 
-class OnboardingCubit extends Cubit<OnboardingState> {
-  final IUserService _userService;
-  final ImageUploader _imageUploader;
-  OnboardingCubit(this._userService, this._imageUploader)
-  // final ILocalCache _localCache;
-  // OnboardingCubit(this._userService, this._imageUploader, this._localCache)
-      : super(OnboardingInitial());
+// class OnboardingCubit extends Cubit<OnboardingState> {
+//   final IUserService _userService;
+//   final ImageUploader _imageUploader;
+//   OnboardingCubit(this._userService, this._imageUploader)
+//   // final ILocalCache _localCache;
+//   // OnboardingCubit(this._userService, this._imageUploader, this._localCache)
+//       : super(OnboardingInitial());
 
-  Future<void> connect(String name, File profileImage) async {
-    emit(Loading());
-    final url = await _imageUploader.uploadImage(profileImage);
-    final user = User(
-      username: name,
-      photoUrl: url,
-      active: true,
-      lastseen: DateTime.now(),
-    );
-    final createdUser = await _userService.connect(user);
-    // final userJson = {
-    //   'username': createdUser.username,
-    //   'active': true,
-    //   'photo_url': createdUser.photoUrl,
-    //   'id': createdUser.id
-    // };
-    // await _localCache.save('USER', userJson);
-    emit(OnboardingSuccess(createdUser));
-  }
-}
+//   Future<void> connect(String name, File profileImage) async {
+//     emit(Loading());
+//     final url = await _imageUploader.uploadImage(profileImage);
+//     final user = User(
+//       username: name,
+//       photoUrl: url,
+//       active: true,
+//       lastseen: DateTime.now(),
+//     );
+//     final createdUser = await _userService.connect(user);
+//     // final userJson = {
+//     //   'username': createdUser.username,
+//     //   'active': true,
+//     //   'photo_url': createdUser.photoUrl,
+//     //   'id': createdUser.id
+//     // };
+//     // await _localCache.save('USER', userJson);
+//     emit(OnboardingSuccess(createdUser));
+//   }
+// }
